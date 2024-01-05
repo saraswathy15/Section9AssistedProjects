aa2f3d60a09b5eadbe8ead97e555aed8cce8cf00
saraswathy15 <charusrinivasan15@gmail.com>
2024-01-05 13:53:39 +0530

    Add project files.

---
+++ Inheritance/Inheritance/SubjectTeacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Inheritance
+{
+    public class SubjectTeacher : Teacher
+    {
+        private Subject whichSubject;
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/BasicGradeCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int getTotalMarks()
+        {
+            return totalMarks;
+        }
+    }
+}
